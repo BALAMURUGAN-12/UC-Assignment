9ecff382781618cc048edb45826abef7b1c50cf0
Balamurugan <balamuruganselvam91803@gmail.com>
2022-03-25 18:16:59 +0530

    add a uc2 program

---
+++ UC CASE/UC2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC_CASE
+{
+    internal class UC2
+    {
+        static void main(string[] args)
+        {
+            //Constants
+            int Is_FullTime = 1;
+            int EmpRatePerHour = 20;
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            //computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == Is_FullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * EmpRatePerHour;
+            Console.WriteLine("Emp wage : " + empWage);
+
+        }
+    }
+}
